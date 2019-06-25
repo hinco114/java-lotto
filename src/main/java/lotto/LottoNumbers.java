21bd05324cdf7c5f8943efec677899621b74ac53
SangHyoun Yun <hainco@gmail.com>
2019-06-25 23:45:00 +0900

    refactor(calculator): LottoNumbers 에 immutable 한 add 추가

--- src/main/java/lotto/LottoNumbers.java
+++ src/main/java/lotto/LottoNumbers.java
@@ -1,11 +1,26 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoNumbers {
     private List<LottoNumber> lottoNumbers;
 
-    public LottoNumbers(List<LottoNumber> lottoNumbers) {
+    public LottoNumbers() {
+        this.lottoNumbers = new ArrayList<>();
+    }
+
+    private LottoNumbers(List<LottoNumber> lottoNumbers, LottoNumber lottoNumber) {
         this.lottoNumbers = lottoNumbers;
+        this.lottoNumbers.add(lottoNumber);
+    }
+
+    public LottoNumbers addLottoNumber(int number) {
+        LottoNumber lottoNumber = new LottoNumber(number);
+        return new LottoNumbers(lottoNumbers, lottoNumber);
+    }
+
+    public int size() {
+        return this.lottoNumbers.size();
     }
 }
--- src/test/java/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoNumbersTest {
     @Test
     @DisplayName("숫자들을 리스트로 입력받으면 LottoNumbers 가 생성된다")
@@ -14,5 +16,15 @@ public class LottoNumbersTest {
         lottoNumberList.add(new LottoNumber(1));
         lottoNumberList.add(new LottoNumber(2));
         LottoNumbers lottoNumbers = new LottoNumbers(lottoNumberList);
+    }
+
+    @Test
+    @DisplayName("로또 숫자를 추가 할 수 있다")
+    void addLottoNumber() {
+        LottoNumbers lottoNumbers = new LottoNumbers();
+        LottoNumbers newLottoNumbers = lottoNumbers.addLottoNumber(1);
+
+        assertThat(lottoNumbers.size()).isEqualTo(0);
+        assertThat(newLottoNumbers.size()).isEqualTo(1);
     }
 }
